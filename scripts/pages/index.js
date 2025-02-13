4abb8578bbfaa0c8f795d023e2dcb6d709bcf38a
Alexis Bouvry <bouvryalexis@gmail.com>
2025-02-13 01:13:55 +0100

    Add search function

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -2,15 +2,41 @@ class Home {
     constructor() {
         this.home = document.querySelector(".card_section")
         this.dataApi = new RecipeApi("./data/recipes.json");
+        this.recipeData = []
+        this.recipeFiltered = []
     }
 
     async main() {
         const recipeData = await this.dataApi.getAllRecipes();
-        recipeData.map(recipe => new Recipe(recipe))
-        .forEach(recipe => {
+        // Affiche l'intégralité du tableau
+        this.recipeData = recipeData.map(recipe => new Recipe(recipe))
+        this.recipeFiltered = this.recipeData
+        this.recipeData.forEach(recipe => {
             const templateRecipe = new RecipeCard(recipe)
             this.home.append(templateRecipe.createRecipeCard())
         })
+    }
+
+    search(value) {
+        this.recipeFiltered = []
+        for (let index = 0; index < this.recipeData.length; index++) {
+            const recipe = this.recipeData[index];
+            let already = false
+            if(recipe.name.toLowerCase().includes(value.toLowerCase())) {
+                this.recipeFiltered.push(recipe)
+                already = true
+            }
+            for (let index = 0; index < recipe.ingredients.length; index++) {
+                const ingredient = recipe.ingredients[index];
+                if(ingredient.ingredient.toLowerCase().includes(value.toLowerCase())) {
+                    if(already == false) {
+                        this.recipeFiltered.push(recipe)
+                        already = true
+                    }
+                }
+            }
+        }
+        console.log(this.recipeFiltered)
     }
 }
 
--- scripts/templates/RecipeCard.js
+++ scripts/templates/RecipeCard.js
@@ -6,9 +6,12 @@ class RecipeCard {
     createRecipeCard() {
         const article = document.createElement("article")
         article.classList.add("recipe_cards")
-        article.setAttribute('id', '${this.data.name}')
+        article.setAttribute('data-id', '${this.data.name}')
         const recipeCard = `
+        <p class="timer">${this.data.time}</p>
+        <div class="card_infos">
         <h2>${this.data.name}</h2>
+        </div>
         `
         article.innerHTML = recipeCard;
         return article;
